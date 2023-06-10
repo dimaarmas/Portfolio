d1e1d9225d0ea15ee068ad095557a608951baf8e
Dumitru Armas <dimaarmas@gmail.com>
2023-06-10 21:35:08 +0200

    added some styles and animation

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -4,7 +4,19 @@ import s from './index.module.css'
 export default function HomePage() {
     return (
         <div className={s.intro}>
-            <div className={s.intro_left}>left</div>
+            <div className={s.intro_left}>
+                <div className={s.intro_left_wrapper}>
+                    <h2 className={s.greetings_text}>Hello, My name is</h2>
+                    <h1 className={s.name_text}>Dumitru Armas</h1>
+                    <div className={s.intro_title}>
+                        <div className={s.intro_title_wrapper}>
+                            <div className={s.intro_title_item}>Junior Web Developer</div>
+                            <div className={s.intro_title_item}>Photographer</div>
+                            <div className={s.intro_title_item}>UI/UX Design</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className={s.intro_right}>right</div>
         </div>
     )
